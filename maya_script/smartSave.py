f377ee33b974bd431c4b813b4a4585815adc17d9
Feng Yusheng <feng.yusheng@mw.mindwalkstudios.com>
2017-04-06 17:28:50 +0800

    modify smartSave

--- maya_script/smartSave.py
+++ maya_script/smartSave.py
@@ -2,39 +2,37 @@
 
 import os
 import re
-
 import pymel.core as pm
 
-scene = pm.system.sceneName()
-file_name = scene.basename().split('.')[0]
-file_path = scene.dirname()
-file_type = scene.getTypeName()[0]
+def smartSave():
+    scene = pm.system.sceneName()
+    scene_path = scene.dirname()
+    file_name = scene.basename().split('.')[0]
+    file_type = scene.getTypeName()[0]
 
-pattern = re.compile(r'_(\d{3})', re.U)
-result = re.search(pattern, file_name)
-if 0 == len(file_name):
-    print('new save')
-elif result is None:
-    file_name = file_name + u'_001'
-else:
-    save_count = int(result.group(1))
-    save_count += 1
+    pattern = re.compile(r'_(\d{3})', re.U)
+    result = re.search(pattern, file_name)
 
-    def _format_save_count(save_count):
-        suffix = unicode(save_count)
-        if save_count < 100 and save_count >= 10:
-            suffix = u'0' + suffix
-        elif save_count < 10 and save_count >= 1:
-            suffix = u'00' + suffix
+    if 0 == len(file_name):
+        print('You need to save your scene first.')
+    elif result is None:
+        file_name = file_name + u'_001'
+        file_name = file_path + '\\' + file_name
+        pm.system.saveAs(file_name, type=file_type)
+    else:
+        def _format_save_count(save_count):
+            suffix = unicode(save_count)
+            if save_count < 100 and save_count >= 10:
+                suffix = u'0' + suffix
+            elif save_count <= 9 and save_count >= 1:
+                suffix = u'00' + suffix
+            return suffix
+        save_count = result.group(1)
+        file_prefix = file_name.partition(save_count)[0]
+        save_count = int(save_count) + 1
+        save_count = _format_save_count(save_count)
+        file_name = file_path + '\\' + file_prefix + save_count
+        pm.system.saveAs(file_name, type=file_type)
 
-        return suffix
-
-    suffix = _format_save_count(save_count)
-    file_name = file_name.partition('_')[0]
-    file_name = file_name + '_' + suffix
-
-def cleanHistory():
-    pm.delete(ch=True)
-
-file_name = file_path + '/' + file_name
-pm.system.saveAs(file_name, type=file_type)
+if __name__ == '__main__':
+    smartSave()
