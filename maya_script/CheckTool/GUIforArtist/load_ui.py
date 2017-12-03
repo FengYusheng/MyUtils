1b2016bc9b4b6cf6397acf5f1eeb0ee3d1d65037
unknown <>
2017-12-03 22:41:03 +0800

    gui for artist

---
+++ maya_script/CheckTool/GUIforArtist/load_ui.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+import sys
+import os
+
+try:
+    from PySide2.QtCore import *
+    from PySide2.QtGui import *
+    from PySide2.QtWidgets import *
+    from pyside2uic import compileUi
+    from PySide2 import __version__
+    from shiboken2 import wrapInstance
+except ImportError:
+    
