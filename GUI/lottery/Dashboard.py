ccc3170fd8f34e51b60cdeeed184ae9b6442ae49
Feng Yusheng <maverick.fengys@gmail.com>
2017-12-14 23:17:35 +0800

    lottery

--- GUI/lottery/Dashboard.py
+++ GUI/lottery/Dashboard.py
@@ -14,3 +14,11 @@ class Dashboard(QWidget):
         painter = QPainter(self)
         painter.setRenderHint(QPainter.Antialiasing, True)
         painter.fillRect(0, 0, self.width(), self.height(), Qt.white)
+
+        painter.save()
+        painter.setRenderHint(QPainter.Antialiasing, True)
+        painter.translate(self.width()/2, self.height()/2)
+        painter.setPen(QPen(QBrush(Qt.darkGray), 3, Qt.SolidLine, Qt.RoundCap, Qt.BevelJoin))
+
+        painter.drawText(0, 0, '你的名字！')
+        painter.restore()
